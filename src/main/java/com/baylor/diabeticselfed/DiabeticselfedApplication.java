1b28a21ac0e4e676a0b724c7cf1ce99c22d7ed18
sakhawathossen58 <sakhawathossen58@gmail.com>
2023-09-19 20:09:36 -0500

    added base project

---
+++ src/main/java/com/baylor/diabeticselfed/DiabeticselfedApplication.java
@@ -0,0 +1,13 @@
+package com.baylor.diabeticselfed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeticselfedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabeticselfedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baylor/diabeticselfed/DiabeticselfedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baylor.diabeticselfed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeticselfedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
