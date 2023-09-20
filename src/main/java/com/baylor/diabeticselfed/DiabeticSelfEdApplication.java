4acc9d1455481419ded99cce1ae3fe61a51e2166
sakhawathossen58 <sakhawathossen58@gmail.com>
2023-09-19 20:57:43 -0500

    added database config and docker

--- src/main/java/com/baylor/diabeticselfed/DiabeticSelfEdApplication.java
+++ src/main/java/com/baylor/diabeticselfed/DiabeticSelfEdApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiabeticselfedApplication {
+public class DiabeticSelfEdApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiabeticselfedApplication.class, args);
+        SpringApplication.run(DiabeticSelfEdApplication.class, args);
     }
 
 }
