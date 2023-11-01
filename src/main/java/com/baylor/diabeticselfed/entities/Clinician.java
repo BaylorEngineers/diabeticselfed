fe38a13f8264c56a5dcf276633f62b6dd740ad6c
Jie Ren <30537105+jieren123@users.noreply.github.com>
2023-11-01 16:44:57 -0500

    m

--- src/main/java/com/baylor/diabeticselfed/entities/Clinician.java
+++ src/main/java/com/baylor/diabeticselfed/entities/Clinician.java
@@ -1,13 +1,27 @@
 package com.baylor.diabeticselfed.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "clinicians")
+//@DiscriminatorValue("CLINICIAN")
+
 public class Clinician extends User {
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
