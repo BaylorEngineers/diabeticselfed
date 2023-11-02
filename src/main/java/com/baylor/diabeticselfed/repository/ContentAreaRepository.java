7804867b825c67d06412f9a919e026d2ed5dcee3
sakhawathossen58 <sakhawathossen58@gmail.com>
2023-11-02 14:21:00 -0500

    added implementation for repo, service, controller

--- src/main/java/com/baylor/diabeticselfed/repository/ContentAreaRepository.java
+++ src/main/java/com/baylor/diabeticselfed/repository/ContentAreaRepository.java
@@ -1,2 +1,9 @@
-package com.baylor.diabeticselfed.repository;public class ContentAreaRepository {
+package com.baylor.diabeticselfed.repository;
+
+import com.baylor.diabeticselfed.entities.ContentArea;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentAreaRepository extends JpaRepository<ContentArea, Integer> {
 }
