e1601512f0f73db1d7d5ea84f8c86fe694a1dad7
miao tingshuo <miaotingshuo@gmail.com>
2023-11-30 19:14:00 -0600

    Back

--- src/main/java/com/baylor/diabeticselfed/controller/ChatGPTController.java
+++ src/main/java/com/baylor/diabeticselfed/controller/ChatGPTController.java
@@ -1,27 +1,27 @@
-//package com.baylor.diabeticselfed.controller;
-//
-//import com.baylor.diabeticselfed.dto.ChatGPTRequest;
-//import com.baylor.diabeticselfed.dto.ChatGPTResponse;
-//import com.baylor.diabeticselfed.service.ChatGPTService;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import reactor.core.publisher.Mono;
-//
-//@RestController
-//@RequestMapping("/api/chatgpt")
-//public class ChatGPTController {
-//
-//    private final ChatGPTService chatGPTService;
-//
-//    public ChatGPTController(ChatGPTService chatGPTService) {
-//        this.chatGPTService = chatGPTService;
-//    }
-//
-//    @PostMapping("/generate-response")
-//    public Mono<ResponseEntity<ChatGPTResponse>> generateResponse(@RequestBody ChatGPTRequest request) {
-//        return chatGPTService.getChatGPTResponse(request)
-//                .map(response -> ResponseEntity.ok(new ChatGPTResponse(response)))
-//                .defaultIfEmpty(ResponseEntity.notFound().build());
-//    }
-//}
+package com.baylor.diabeticselfed.controller;
+
+import com.baylor.diabeticselfed.dto.ChatGPTRequest;
+import com.baylor.diabeticselfed.dto.ChatGPTResponse;
+import com.baylor.diabeticselfed.service.ChatGPTService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/chatgpt")
+public class ChatGPTController {
+
+    private final ChatGPTService chatGPTService;
+
+    public ChatGPTController(ChatGPTService chatGPTService) {
+        this.chatGPTService = chatGPTService;
+    }
+
+    @PostMapping("/generate-response")
+    public Mono<ResponseEntity<ChatGPTResponse>> generateResponse(@RequestBody ChatGPTRequest request) {
+        return chatGPTService.getChatGPTResponse(request)
+                .map(response -> ResponseEntity.ok(new ChatGPTResponse(response)))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+}
--- src/main/java/com/baylor/diabeticselfed/service/ChatGPTService.java
+++ src/main/java/com/baylor/diabeticselfed/service/ChatGPTService.java
@@ -1,80 +1,80 @@
-//package com.baylor.diabeticselfed.service;
-//
-//import com.baylor.diabeticselfed.dto.ChatGPTRequest;
-//import com.baylor.diabeticselfed.dto.OpenAIRequest;
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.reactive.function.client.WebClient;
-//import reactor.core.publisher.Mono;
-//
-//@Service
-//public class ChatGPTService {
-//
-//    private final WebClient webClient;
-//
-//    @Value("${openai.api.url}")
-//    private String chatGPTApiUrl;
-//
-//    @Value("${openai.api.key}")
-//    private String openaiApiKey;
-//
-//    public ChatGPTService(WebClient.Builder webClientBuilder) {
-//        this.webClient = webClientBuilder.baseUrl(chatGPTApiUrl).build();
-//    }
-//
-//    public Mono<String> getChatGPTResponse(ChatGPTRequest request) {
-//
-//        System.out.println(request.getName() + " " + request.getBackground() + " " + request.getOccupation());
-//        System.out.println(request.getInterests() + " " + request.getAge());
-//
-//        String prompt = buildPrompt(request);
-//        System.out.println(prompt);
-//        OpenAIRequest openAIRequest = new OpenAIRequest("gpt-3.5-turbo", prompt, 150);
-//
-//        String curlCommand = buildCurlCommand(openAIRequest);
-//
-//        System.out.println(curlCommand);
-//
-//        return webClient.post()
-//                .uri("/v1/engines/davinci-codex/completions")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .header("Authorization", "Bearer " + openaiApiKey)
-//                .bodyValue(openAIRequest)
-//                .retrieve()
-//                .bodyToMono(String.class);
-//    }
-//
-//    private String buildPrompt(ChatGPTRequest request) {
-//        return String.format(
-//                "Here is a profile:\n- Name: %s\n- Age: %d\n- Education: %s\n- Occupation: %s\n- Interests: %s\n- Background: %s\n- Goal: %s\n\n"
-//                        + "Based on this profile, provide a motivational message as the user did not adhere to a healthy diet in the past few days.",
-//                request.getName(),
-//                request.getAge(),
-//                request.getEducation(),
-//                request.getOccupation(),
-//                request.getInterests(),
-//                request.getBackground(),
-//                request.getGoal()
-//        );
-//    }
-//
-//    private String buildCurlCommand(OpenAIRequest openAIRequest) {
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            String jsonBody = objectMapper.writeValueAsString(openAIRequest);
-//            String escapedJsonBody = jsonBody.replace("\"", "\\\"");
-//
-//            return "curl -X POST " +
-//                    chatGPTApiUrl + "/v1/engines/davinci-codex/completions " +
-//                    "-H \"Content-Type: application/json\" " +
-//                    "-H \"Authorization: Bearer " + openaiApiKey + "\" " +
-//                    "-d \"" + escapedJsonBody + "\"";
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//            return "Error building cURL command";
-//        }
-//    }
-//}
+package com.baylor.diabeticselfed.service;
+
+import com.baylor.diabeticselfed.dto.ChatGPTRequest;
+import com.baylor.diabeticselfed.dto.OpenAIRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ChatGPTService {
+
+    private final WebClient webClient;
+
+    @Value("${openai.api.url}")
+    private String chatGPTApiUrl;
+
+    @Value("${openai.api.key}")
+    private String openaiApiKey;
+
+    public ChatGPTService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(chatGPTApiUrl).build();
+    }
+
+    public Mono<String> getChatGPTResponse(ChatGPTRequest request) {
+
+        System.out.println(request.getName() + " " + request.getBackground() + " " + request.getOccupation());
+        System.out.println(request.getInterests() + " " + request.getAge());
+
+        String prompt = buildPrompt(request);
+        System.out.println(prompt);
+        OpenAIRequest openAIRequest = new OpenAIRequest("gpt-3.5-turbo", prompt, 150);
+
+        String curlCommand = buildCurlCommand(openAIRequest);
+
+        System.out.println(curlCommand);
+
+        return webClient.post()
+                .uri("/v1/engines/davinci-codex/completions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + openaiApiKey)
+                .bodyValue(openAIRequest)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    private String buildPrompt(ChatGPTRequest request) {
+        return String.format(
+                "Here is a profile:\n- Name: %s\n- Age: %d\n- Education: %s\n- Occupation: %s\n- Interests: %s\n- Background: %s\n- Goal: %s\n\n"
+                        + "Based on this profile, provide a motivational message as the user did not adhere to a healthy diet in the past few days.",
+                request.getName(),
+                request.getAge(),
+                request.getEducation(),
+                request.getOccupation(),
+                request.getInterests(),
+                request.getBackground(),
+                request.getGoal()
+        );
+    }
+
+    private String buildCurlCommand(OpenAIRequest openAIRequest) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonBody = objectMapper.writeValueAsString(openAIRequest);
+            String escapedJsonBody = jsonBody.replace("\"", "\\\"");
+
+            return "curl -X POST " +
+                    chatGPTApiUrl + "/v1/engines/davinci-codex/completions " +
+                    "-H \"Content-Type: application/json\" " +
+                    "-H \"Authorization: Bearer " + openaiApiKey + "\" " +
+                    "-d \"" + escapedJsonBody + "\"";
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "Error building cURL command";
+        }
+    }
+}
