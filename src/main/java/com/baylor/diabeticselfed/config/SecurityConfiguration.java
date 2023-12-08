63602f6bbfbeafd3cb04238f6e15c4e6d53f757f
sakhawathossen58 <sakhawathossen58@gmail.com>
2023-12-07 19:09:02 -0600

    updated test URL

--- src/main/java/com/baylor/diabeticselfed/config/SecurityConfiguration.java
+++ src/main/java/com/baylor/diabeticselfed/config/SecurityConfiguration.java
@@ -29,7 +29,9 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+    private static final String[] WHITE_LIST_URL = {
+            "/",
+            "/api/v1/auth/**",
             "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
--- src/main/java/com/baylor/diabeticselfed/controller/DefaultController.java
+++ src/main/java/com/baylor/diabeticselfed/controller/DefaultController.java
@@ -1,9 +1,11 @@
 package com.baylor.diabeticselfed.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/clinicians")
 public class DefaultController {
 
     @GetMapping("/")
